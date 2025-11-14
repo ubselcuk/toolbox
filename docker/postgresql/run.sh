379ab62997afbab810b9980321364849728c1ea8
ubselcuk <ubselcuk@gmail.com>
2025-11-14 17:26:38 +0300

    Update PostgreSQL run script with enhanced resource management and health checks.

--- docker/postgresql/run.sh
+++ docker/postgresql/run.sh
@@ -3,13 +3,22 @@ set -euo pipefail
 
 docker run \
   -d \
-  --rm \
   --name postgres \
   --restart unless-stopped \
-  -p 5432:5432 \
+  --network custom-app-network \
+  --cpus="1.0" \
+  --memory="4g" \
+  -p 127.0.0.1:5432:5432 \
   -e TZ=Europe/Istanbul \
   -e POSTGRES_USER="foo" \
   -e POSTGRES_PASSWORD="bar" \
   -e POSTGRES_DB="default" \
-  -v pgdata:/var/lib/postgresql/data \
+  -v pgdb:/var/lib/postgresql/data \
+  --health-cmd "pg_isready -U foo" \
+  --health-interval 5s \
+  --health-timeout 5s \
+  --health-retries 5 \
+  --log-driver=json-file \
+  --log-opt max-size=50m \
+  --log-opt max-file=3 \
   postgres:latest
