396c6a549799c04bf0ef8cc4a53a2cbca2d85a8a
ubselcuk <ubselcuk@gmail.com>
2025-11-16 01:42:20 +0300

    Update PostgreSQL run script.

--- docker/postgresql/run.sh
+++ docker/postgresql/run.sh
@@ -10,15 +10,14 @@ docker run \
   --memory="4g" \
   -p 127.0.0.1:5432:5432 \
   -e TZ=Europe/Istanbul \
-  -e POSTGRES_USER="foo" \
-  -e POSTGRES_PASSWORD="bar" \
-  -e POSTGRES_DB="default" \
-  -v pgdb:/var/lib/postgresql/data \
-  --health-cmd "pg_isready -U foo" \
+  -e POSTGRES_USER="postgres" \
+  -e POSTGRES_PASSWORD="foobar" \
+  -v pgdb:/var/lib/postgresql \
+  --health-cmd "pg_isready -U postgres" \
   --health-interval 5s \
   --health-timeout 5s \
   --health-retries 5 \
   --log-driver=json-file \
   --log-opt max-size=50m \
   --log-opt max-file=3 \
-  postgres:latest
+  postgres:18
